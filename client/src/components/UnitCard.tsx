7226b0e88fd97a79e570fcbf10f724e48a2e3ff3
jbraczyk <bouratchikj@gmail.com>
2022-11-22 14:34:21 +0100

    added fetch call for the sound playing

--- client/src/components/UnitCard.tsx
+++ client/src/components/UnitCard.tsx
@@ -1,16 +1,27 @@
 import { UnitType } from '../types'
 import './styles/UnitCard.css'
 
+const callUrl = 'http://localhost:4000/soundCall'
+
 type UnitProps = { unit: UnitType }
 
 export const UnitCard = ({ unit }: UnitProps) => {
 
-  async function handleCall(e) {
-    console.log(unit.slug)
+  async function handleCall() {
+    fetch(callUrl, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({ name: unit.slug }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }
+     }).then(res => console.log(res))
+
   }
 
   return (
-    <div className="unit" onClick={(e) => handleCall(e)}>
+    <div className="unit" onClick={() => handleCall()}>
         <p>{unit.name}</p>
         <img src={unit.imgLink}></img>
     </div>
